fdbc2febc9f3bc26244f28bb06b45a02b586c234
Priyanshu Tyagi <110727059+Priyanshu282002@users.noreply.github.com>
2024-07-10 16:14:34 +0530

    Corrected verify link

--- Controllers/AuthController.js
+++ Controllers/AuthController.js
@@ -215,7 +215,7 @@ const Sendmail = async (FirstName, Email, UserId) => {
                     <table border="0" cellpadding="0" cellspacing="0">
                       <tr>
                         <td align="center" bgcolor="#1a82e2" style="border-radius: 6px;">
-                          <a href="http://localhost:4000/api/v1/Auth/VerifyUser/${hashId}" 
+                          <a href="https://bit-code-backend-one.vercel.app/api/v1/Auth/VerifyUser/${hashId}" 
                           target="_blank" style="display: inline-block; padding: 16px 36px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; color: #ffffff; text-decoration: none; border-radius: 6px;">Verify Email</a>
                         </td>
                       </tr>
